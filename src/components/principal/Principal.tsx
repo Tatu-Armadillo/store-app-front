dbd88b73b491bb6d0cfa0c52d72f217c9a1a076f
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2022-03-27 20:49:57 -0700

    init pagina principal

---
+++ src/components/principal/Principal.tsx
@@ -0,0 +1,14 @@
+
+const Principal = () => {
+
+    return (
+        <main>
+            <button className='btn btn-outline-warning'>Cliente</button>
+            <button className='btn btn-outline-success'>Fornecedor</button>
+            <button className='btn btn-outline-danger'>Produto</button>
+            <button className='btn btn-outline-primary'>Venda</button>
+        </main>
+    );
+}
+
+export default Principal;
